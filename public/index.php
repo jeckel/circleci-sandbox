3db89e2c7b61aa2e7469da429632678023221698
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-03-25 08:57:42 +0100

    :white_check_mark: Upgrade with slim

--- public/index.php
+++ public/index.php
@@ -3,16 +3,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
 include '../c3.php';
 
-include '../src/Hello.php';
+$app = new \Slim\App;
+$app->get('/hello', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello World");
+    return $response;
+});
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = ($args['name'] == 'bob') ? 'Bob Marley' : $args['name'];
 
-$hello = new Hello($_GET['name']);
-?>
-<html>
-<head></head>
-<body>
-<?= $hello ?>
-</body>
-</html>
+    $response->getBody()->write("Hello, $name");
 
+    return $response;
+});
+$app->run();
--- tests/acceptance/homepage.feature
+++ tests/acceptance/homepage.feature
@@ -2,9 +2,10 @@ Feature: Homepage
   Check home page rendering
 
   Scenario: try homepage
-    Given I am on "/"
+    Given I am on "/hello"
     Then I should see "Hello World"
 
   Scenario: try homepage bob
-    Given I am on "/?name=bob"
-    Then I should see "Hello bob"
+    Given I am on "/hello/bob"
+    Then I should see "Hello, Bob Marley"
+
