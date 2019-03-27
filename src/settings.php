a0576fbf80db614bd223be31f31f5eb366e081c7
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-03-27 10:34:26 +0100

    :construction: Configure dependencies logger and respotories

--- src/settings.php
+++ src/settings.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
  * User: jeckel
  * Date: 27/03/19
  * Time: 08:57
@@ -9,9 +8,16 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+define('APP_ROOT', __DIR__);
 
 return [
     'settings' => [
         'displayErrorDetails' => true,
+        // Monolog settings
+        'logger' => [
+            'name'  => 'Sample-CI',
+            'path'  => 'php://stdout',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
     ],
 ];
