f36105409a8c66a040537a8f0af666e1c2fe4ad6
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-03-27 19:18:40 +0100

    :bulb: Add comments

--- tests/unit/Entity/UserTest.php
+++ tests/unit/Entity/UserTest.php
@@ -1,15 +1,23 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
  * User: jeckel
  * Date: 27/03/19
  * Time: 19:08
  */
+namespace Test\App\Entity;
 
 use App\Entity\User;
 
+/**
+ * Class UserTest
+ * @package Test\App\Entity
+ */
 class UserTest extends \Codeception\Test\Unit
 {
+    /**
+     * @test User::setDisplayName
+     * @test User::getDisplayName
+     */
     public function testSetGetDisplayName()
     {
         $user = new User();
@@ -17,6 +25,10 @@ class UserTest extends \Codeception\Test\Unit
         $this->assertEquals('foobar', $user->getDisplayName());
     }
 
+    /**
+     * @test User::setId
+     * @test USer::getId
+     */
     public function testSetGetId()
     {
         $user = new User();
@@ -24,6 +36,10 @@ class UserTest extends \Codeception\Test\Unit
         $this->assertEquals(123, $user->getId());
     }
 
+    /**
+     * @test User::setLogin
+     * @test User::getLogin
+     */
     public function testSetGetLogin()
     {
         $user = new User();
